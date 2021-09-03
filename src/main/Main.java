52f6096214b71cf03c2257f1a07b2750f751f1f3
katicabogar87 <katicabogar87@gmail.com>
2021-09-03 19:39:35 +0200

    main method instances

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,31 @@
 package main;
 
+import main.model.Gender;
+import main.model.animals.Animal;
+import main.model.animals.Cat;
+import main.model.animals.Dog;
+import main.model.animals.Parrot;
+import main.model.humanoids.Human;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO
+
+
+        Human human = new Human("Gergely", "Lorincz", LocalDate.parse("1985-12-29"), Gender.MALE);
+
+        Animal animal = new Animal();
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Parrot parrot = new Parrot();
+
+    human.getPetsOwned().add(animal);
+    human.getPetsOwned().add(dog);
+    human.getPetsOwned().add(cat);
+    human.getPetsOwned().add(parrot);
+
     }
 }
