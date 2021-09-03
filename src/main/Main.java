e117c38d8de7e55bb76a90331f53b89fec057bfb
GergelyLorincz <89002426+GergelyLorincz@users.noreply.github.com>
2021-09-03 19:37:33 +0200

    Added human object and animal objects

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,30 @@
 package main;
 
+import main.model.animals.Cat;
+import main.model.animals.Dog;
+import main.model.animals.Parrot;
+import main.model.humanoids.Human;
+import main.model.humanoids.Humanoid;
+
+import java.nio.channels.Pipe;
+import java.time.LocalDate;
+
+import static main.model.Gender.FEMALE;
+
 public class Main {
 
     public static void main(String[] args) {
-        // TODO
+
+        Human human = new Human("Kata", "Antal", LocalDate.parse("1987.08.11"), FEMALE);
+
+
+        Dog kutyuli = new Dog();
+        Cat morcos = new Cat();
+        Parrot pirate = new Parrot();
+
+        human.getPetsOwned().add(kutyuli);
+        human.getPetsOwned().add(morcos);
+        human.getPetsOwned().add(pirate);
     }
+
 }
--- src/main/model/humanoids/Human.java
+++ src/main/model/humanoids/Human.java
@@ -2,10 +2,17 @@ package main.model.humanoids;
 
 import main.model.Gender;
 
+import java.time.LocalDate;
+
 public class Human extends Humanoid {
 
     private Gender gender;
- 
+
+    public Human(String lastName, String firstName, LocalDate birth, Gender gender) {
+        super(lastName, firstName, birth);
+        this.gender = gender;
+    }
+
     public Gender getGender() {
         return gender;
     }
