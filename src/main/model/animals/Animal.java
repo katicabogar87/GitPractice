14de8312d00eba2f4892734b532cd10deec48db3
GergelyLorincz <89002426+GergelyLorincz@users.noreply.github.com>
2021-09-03 18:52:44 +0200

    Add Animal eat method

--- src/main/model/animals/Animal.java
+++ src/main/model/animals/Animal.java
@@ -25,19 +25,69 @@ public class Animal implements AnimalInterface {
         owner = this.owner;
     }
 
-    @Override
-    public void eat() {
-        // TODO
+
+    public void eat(Food food) {
+        if (food.equals(favoriteFood)) {
+            isHungry = false;
+        } else
+            isHungry = true;
     }
 
     @Override
     public void makeSound() {
-        // TODO
+
     }
 
     @Override
     public void play() {
-        // TODO
+
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public String getFavoriteToy() {
+        return favoriteToy;
+    }
+
+    public void setFavoriteToy(String favoriteToy) {
+        this.favoriteToy = favoriteToy;
+    }
+
+    public Food getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(Food favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public boolean isHungry() {
+        return isHungry;
+    }
+
+    public void setHungry(boolean hungry) {
+        isHungry = hungry;
+    }
+
+    public Humanoid getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Humanoid owner) {
+        this.owner = owner;
+    }
 }
